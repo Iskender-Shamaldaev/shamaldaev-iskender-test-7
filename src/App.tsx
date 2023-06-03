81df8e3efd990e6145be0e42a95080cff4c1444f
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-06-03 15:29:47 +0600

    Создал тип, функции удаления и добавления продукта.

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,97 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import foodImage from './assets/food.png';
 import drinkImage from './assets/drinks.png';
 import './App.css';
-import {nanoid} from "nanoid";
+import { nanoid } from 'nanoid';
+import {IProduct} from "./types";
 
 const App = () => {
+    const [products, setProducts] = useState<IProduct[]>([
+        { id: nanoid(), name: 'Hamburger', price: 80, image: foodImage, count: 0 },
+        { id: nanoid(), name: 'Cheeseburger', price: 90, image: foodImage, count: 0  },
+        { id: nanoid(), name: 'Fries', price: 45, image: foodImage, count: 0 },
+        { id: nanoid(), name: 'Coffee', price: 70, image: drinkImage, count: 0  },
+        { id: nanoid(), name: 'Tea', price: 50, image: drinkImage, count: 0  },
+        { id: nanoid(), name: 'Cola', price: 40, image: drinkImage, count: 0  },
+    ]);
 
-const [products, setProducts] = useState([
-    { id: nanoid(),name: 'Hamburger', price: 80, image: foodImage },
-    { id: nanoid(), name: 'Cheeseburger', price: 90, image:foodImage },
-    { id: nanoid(), name: 'Fries', price: 45, image: foodImage },
-    { id:nanoid(),name: 'Coffee', price: 70, image: drinkImage },
-    { id:nanoid(),name: 'Tea', price: 50, image: drinkImage  },
-    { id:nanoid(),name: 'Cola', price: 40, image: drinkImage  },
-]);
+    const [order, setOrder] = useState<IProduct[]>([]);
 
-const [order, setOrder] = useState('Order is empty');
+    const [totalPrice, setTotalPrice] = useState(0);
+
+    const buttonClick = (id: string) => {
+        const selected = products.find((item) => item.id === id);
+        if (selected) {
+            const existingItem = order.find((item) => item.id === id);
+            if (existingItem) {
+                const updatedOrder = order.map((item) => {
+                    if (item.id === id) {
+                        return {
+                            ...item,
+                            count: item.count + 1,
+                        };
+                    }
+                    return item;
+                });
+                setOrder(updatedOrder);
+            } else {
+                const newOrderItem = {
+                    ...selected,
+                    count: 1,
+                };
+                setOrder((prevOrder) => [...prevOrder, newOrderItem]);
+            }
+            setTotalPrice((prevPrice) => prevPrice + selected.price);
+        }
+    };
+
+    const removeClick = (id: string, price: number, count: number) => {
+        const updatedOrder = order.map((item) => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    count: 0,
+                };
+            }
+            return item;
+        });
+
+        const filteredOrder = updatedOrder.filter((item) => item.count > 0);
+        setOrder(filteredOrder);
+
+        const removedProductPrice = price * count;
+        setTotalPrice((prevPrice) => prevPrice - removedProductPrice);
+    };
 
 
-
-
-  return (
-      <div className="App">
-          <div className="left">
-              {order}
-          </div>
-          <div style={{border: "1px solid black"}}>
-              {
-                  products.map(item => (
-                      <div style={{display: "flex", alignItems: "center", border:"1px solid black"}}>
-                          <button style={{width: "100%"}}>
-                              <img style={{width: "75px"}} src={item.image} alt={item.name}/>
-                              <div>
-                                  <strong>{item.name}</strong>
-                                  <p>
-                                      Price:{item.price}KGS
-                                  </p>
-                              </div>
-                          </button>
-                      </div>
-                  ))
-              }
-          </div>
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="left">
+                <div>
+                    <strong>Total price: {totalPrice} KGS</strong>
+                </div>
+                {order.map((item) => (
+                    <div key={item.id}>
+                        {item.name} - {item.count}
+                        <button onClick={() => removeClick(item.id, item.price, item.count)}>X</button>
+                    </div>
+                ))}
+            </div>
+            <div style={{ border: '1px solid black' }}>
+                {products.map((item) => (
+                    <div key={item.id} style={{ display: 'flex', alignItems: 'center', border: '1px solid black' }}>
+                        <button style={{ width: '100%' }} onClick={() => buttonClick(item.id)}>
+                            <img style={{ width: '75px' }} src={item.image} alt={item.name} />
+                            <div>
+                                <strong>{item.name}</strong>
+                                <p>Price: {item.price} KGS</p>
+                            </div>
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default App;
