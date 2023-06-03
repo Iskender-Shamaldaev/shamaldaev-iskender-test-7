88e1d530eaeaca1b550146f4c91aefb7cad61151
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-06-03 12:46:55 +0600

    Создал стейт и вывил на экран.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import foodImage from './assets/food.png';
+import drinkImage from './assets/drinks.png';
 import './App.css';
+import {nanoid} from "nanoid";
 
-function App() {
+const App = () => {
+
+const [products, setProducts] = useState([
+    { id: nanoid(),name: 'Hamburger', price: 80, image: foodImage },
+    { id: nanoid(), name: 'Cheeseburger', price: 90, image:foodImage },
+    { id: nanoid(), name: 'Fries', price: 45, image: foodImage },
+    { id:nanoid(),name: 'Coffee', price: 70, image: drinkImage },
+    { id:nanoid(),name: 'Tea', price: 50, image: drinkImage  },
+    { id:nanoid(),name: 'Cola', price: 40, image: drinkImage  },
+]);
+
+const [order, setOrder] = useState('Order is empty');
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="left">
+              {order}
+          </div>
+          <div style={{border: "1px solid black"}}>
+              {
+                  products.map(item => (
+                      <div style={{display: "flex", alignItems: "center", border:"1px solid black"}}>
+                          <button style={{width: "100%"}}>
+                              <img style={{width: "75px"}} src={item.image} alt={item.name}/>
+                              <div>
+                                  <strong>{item.name}</strong>
+                                  <p>
+                                      Price:{item.price}KGS
+                                  </p>
+                              </div>
+                          </button>
+                      </div>
+                  ))
+              }
+          </div>
+      </div>
   );
-}
+};
 
 export default App;
