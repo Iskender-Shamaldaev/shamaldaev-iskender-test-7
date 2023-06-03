bf6f35c20265aca27480c94ff97186c8301d079b
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-06-03 18:29:04 +0600

    Сделал корректировку и добавил новые картинки, изменил стили.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import React, {useState} from 'react';
-import foodImage from './assets/food.png';
-import drinkImage from './assets/drinks.png';
-import {nanoid} from 'nanoid';
-import {IProduct} from "./types";
+import React, { useState } from 'react';
+import hamburgerImage from './assets/hamburger.jpg';
+import cheeseburgerImage from './assets/cheeseburger.jpg';
+import friesImage from './assets/fries.png';
+import coffeeImage from './assets/cofee.png';
+import teaImage from './assets/tea.png';
+import colaImage from './assets/cola.png';
+import { nanoid } from 'nanoid';
+import { IProduct } from "./types";
 import Order from "./Order/Order";
 import ProductList from "./ProductList/ProductList";
 import './App.css';
 
-
 const App = () => {
-    const [products] = useState<IProduct[]>([
-        {id: nanoid(), name: 'Hamburger', price: 80, image: foodImage, count: 0},
-        {id: nanoid(), name: 'Cheeseburger', price: 90, image: foodImage, count: 0},
-        {id: nanoid(), name: 'Fries', price: 45, image: foodImage, count: 0},
-        {id: nanoid(), name: 'Coffee', price: 70, image: drinkImage, count: 0},
-        {id: nanoid(), name: 'Tea', price: 50, image: drinkImage, count: 0},
-        {id: nanoid(), name: 'Cola', price: 40, image: drinkImage, count: 0},
+    const [products, setProducts] = useState<IProduct[]>([
+        { id: nanoid(), name: 'Hamburger', price: 80, image: hamburgerImage, count: 0 },
+        { id: nanoid(), name: 'Cheeseburger', price: 90, image: cheeseburgerImage, count: 0 },
+        { id: nanoid(), name: 'Fries', price: 45, image: friesImage, count: 0 },
+        { id: nanoid(), name: 'Coffee', price: 70, image: coffeeImage, count: 0 },
+        { id: nanoid(), name: 'Tea', price: 50, image: teaImage, count: 0 },
+        { id: nanoid(), name: 'Cola', price: 40, image: colaImage, count: 0 },
     ]);
 
     const [order, setOrder] = useState<IProduct[]>([]);
-
     const [totalPrice, setTotalPrice] = useState(0);
-
 
     const buttonClick = (id: string) => {
         const selected = products.find((item) => item.id === id);
@@ -67,11 +68,14 @@ const App = () => {
         setTotalPrice((prevState) => prevState - removedProductPrice);
     };
 
+    const updateProductState = (updatedProducts: IProduct[]) => {
+        setProducts(updatedProducts);
+    };
 
     return (
         <div className="App">
-            <Order order={order} totalPrice={totalPrice} removeClick={removeClick}/>
-            <ProductList products={products} buttonClick={buttonClick}/>
+            <Order order={order} totalPrice={totalPrice} removeClick={removeClick} />
+            <ProductList products={products} buttonClick={buttonClick} updateProductState={updateProductState} />
         </div>
     );
 };
--- src/ProductList/ProductList.tsx
+++ src/ProductList/ProductList.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {IProduct} from "../types";
+import { IProduct } from "../types";
 
 interface ProductListProps {
     products: IProduct[];
     buttonClick: (id: string) => void;
+    updateProductState: (updatedProducts: IProduct[]) => void;
 }
 
-const ProductList: React.FC<ProductListProps> = ({products, buttonClick}) => {
+const ProductList: React.FC<ProductListProps> = ({ products, buttonClick, updateProductState }) => {
     return (
-        <div style={{border: '1px solid black'}}>
+        <div style={{ border: '1px solid black', display: "flex", width: "500px", flexWrap: "wrap", justifyContent: "space-around" }}>
             {products.map((item) => (
-                <div key={item.id} style={{border: '1px solid black'}}>
-                    <button style={{width: '100%'}} onClick={() => buttonClick(item.id)}>
-                        <img style={{width: '75px'}} src={item.image} alt={item.name}/>
+                <div key={item.id} style={{ border: '1px solid black', margin: "27px" }}>
+                    <button style={{ width: '100%' }} onClick={() => buttonClick(item.id)}>
+                        <img style={{ width: '75px' }} src={item.image} alt={item.name} />
                         <div>
                             <strong>{item.name}</strong>
                             <p>Price: {item.price} KGS</p>
